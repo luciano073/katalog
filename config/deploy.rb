b1f4ccf2de3df4b6aa01a3a32e451e7b04685fe7
Luciano Aquino <luciano073@gmail.com>
2014-01-19 14:33:11 +0000

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,13 @@ set :repository,  "git://github.com/luciano073/katalog.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :user, "vagrant"
-set :deploy_to, "/home/vagrant/katalog/apps/#{application}"
+set :deploy_to, "/home/vagrant/apps/#{application}"
 
 role :web, "localhost"                          # Your HTTP server, Apache/etc
 role :app, "localhost"                          # This may be the same as your `Web` server
 role :db,  "localhost", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
-set :port, 22
+# set :port, 22
 set :keep_releases, 5
 set :use_sudo, false
 # if you want to clean up old releases on each deploy uncomment this:
