da756651d7ad885d707d01bf0337a9ea010c2180
luciano073 <luciano073@gmail.com>
2014-02-08 00:16:14 -0200

    assets precompile continue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Katalog::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( lightbox.css jquery.bxslider.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
