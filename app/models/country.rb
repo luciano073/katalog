a0b35a819fc4243201f1ba2c53b6d83917115919
luciano073 <luciano073@gmail.com>
2014-03-29 22:26:08 -0300

    fix country index

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,7 @@ class Country < ActiveRecord::Base
   has_many :artists
   has_many :films
   has_many :media, :class_name => "medium"
-
+  before_save :name_normalize
   mount_uploader :flag, FlagUploader
   paginates_per 10
   # scope :finder, lambda { |q| where("i_unaccent(countries.name) ilike
@@ -19,4 +19,17 @@ class Country < ActiveRecord::Base
   def url_16
     self.flag.url(:_16)
   end
+end
+
+def name_normalize
+    # re = /\s+[ixv]{,3}\z/i # for names ends with II, III and so on
+    if self.name
+      self.name = self.name.strip.squeeze(' ').titleize
+      # str =~ re
+      # self.name = str.sub(re, $~.to_s.upcase)      
+    end
+
+    if self.english_name
+      self.english_name = self.english_name.strip.squeeze(' ').titleize
+    end
 end
