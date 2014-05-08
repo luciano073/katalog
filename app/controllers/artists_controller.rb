95765b26769f4fc46a9cb6f668dfef98a7ace68c
luciano073 <luciano073@gmail.com>
2014-05-07 21:11:10 -0300

    fix controller error

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  # respond_to :html, :json
+  respond_to :html, :json
   include ActionView::Helpers::NumberHelper
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
   
